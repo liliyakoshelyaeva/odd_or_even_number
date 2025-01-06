087ff5a06c1f63edae929d992f5609570839f783
liliyakoshelyaeva <146521731+liliyakoshelyaeva@users.noreply.github.com>
2025-01-06 18:44:37 +0300

    Add a programm

---
+++ odd_or_even.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odd_or_even
+{
+    internal class odd_or_even
+    {
+        public static void Main()
+        {
+            Console.Write("Введите целое число: ");
+            if(int.TryParse(Console.ReadLine(), out int number)) 
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"Число {number} является чётным");
+                }
+                else
+                {
+                    Console.WriteLine($"Число {number} является нечётным");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число");
+            }
+        }
+    }
+}
